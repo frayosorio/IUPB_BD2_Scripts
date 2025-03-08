82ecedf338ac6edab54c7dd45d4fb44fb526a8af
fray osorio <frayosorio@gmail.com>
2025-03-08 08:23:30 -0500

    Se completa TRIGGER para garantizar que solo haya una capital de REGION

--- Consultas Verificacion TRIGGERS - Division Politica.sql
+++ Consultas Verificacion TRIGGERS - Division Politica.sql
@@ -6,11 +6,25 @@ SELECT R.Nombre, C.Nombre Ciudad, C.CapitalRegion, C.Id IdCiudad, R.Id IdRegion
 	WHERE P.Nombre = 'Colombia'
 		AND C.CapitalRegion=1
 
+--Listar las capitales de Departamento de Colombia (optimizada)
+SELECT R.Nombre, R.Id IdRegion,
+	CASE WHEN EXISTS(SELECT 1 FROM Ciudad WHERE CapitalRegion=1 AND IdRegion=R.Id)
+	THEN
+		(SELECT Nombre FROM Ciudad WHERE CapitalRegion=1 AND IdRegion=R.Id)
+	ELSE
+		'** Sin Capital **'
+	END Capital
+	FROM Region R
+		JOIN Pais P ON P.Id=R.IdPais
+	WHERE P.Nombre = 'Colombia'
+
+
 --Contar las ciudades de Antioquia
 SELECT COUNT(*)
 	FROM Ciudad C
 		JOIN Region R ON C.IdRegion=R.Id
 	WHERE R.Nombre='Antioquia'
+		AND C.CapitalRegion=1
 
 --Volver capital de Antioquia a Bello
 UPDATE Ciudad
@@ -21,6 +35,14 @@ UPDATE Ciudad
 UPDATE Ciudad
 	SET CapitalRegion=1
 	WHERE IdRegion=2
+
+--Volver capital a todas las ciudades de Colombia (ERROR)
+UPDATE Ciudad
+	SET CapitalRegion=1
+	FROM Pais P 
+		JOIN Region R ON P.Id=R.IdPais
+		JOIN Ciudad C ON C.IdRegion=R.Id
+	WHERE P.Nombre='Colombia'
 
 --Volver capital a 'San Antonio de Pereira'
 INSERT INTO Ciudad	
@@ -33,3 +55,20 @@ INSERT INTO Ciudad
 	VALUES
 	('Santa Helena', 2, 1),
 	('San Antonio de Prado', 2, 1)
+
+--Listar Ciudades de Antioquia
+SELECT R.Nombre, C.Nombre Ciudad, C.CapitalRegion, C.Id IdCiudad, R.Id IdRegion
+	FROM Ciudad C
+		JOIN Region R ON C.IdRegion=R.Id
+		JOIN Pais P ON P.Id=R.IdPais
+	WHERE P.Nombre = 'Colombia'
+		AND R.Nombre='Antioquia'
+
+--Quitar la capital de Antioquia
+UPDATE Ciudad
+	SET CapitalRegion=0
+	WHERE IdRegion=2
+
+--Eliminar Ciudades agregadas para pruebas
+DELETE FROM Ciudad	
+	WHERE Nombre IN ('San Antonio de Pereira', 'Santa Helena', 'San Antonio de Prado')
--- TRIGGERS - Division Politica.sql
+++ TRIGGERS - Division Politica.sql
@@ -1,3 +1,4 @@
+--version que no deja capital de region ante ambiguedades
 CREATE TRIGGER tActualizarCapitalRegion
 ON Ciudad
 FOR INSERT, UPDATE
@@ -5,6 +6,19 @@ AS BEGIN
 	--Validar que se esta actualizando una capital de region
 	IF EXISTS(SELECT * FROM Inserted WHERE CapitalRegion=1)
 	BEGIN
+		-- Verificar si la inserción o actualización causaría que haya más de una capital de región
+		IF EXISTS(SELECT 1
+					FROM Inserted I
+						JOIN Ciudad C ON I.IdRegion=C.IdRegion
+					WHERE I.CapitalRegion=1 AND C.CapitalRegion=1 AND C.Id<>I.Id
+					GROUP BY I.IdRegion
+					HAVING COUNT(*) > 1)
+		BEGIN
+			RAISERROR('No se acepta más de una capital por región', 16, 1)
+			ROLLBACK TRANSACTION
+		END
+
+		-- Si se está estableciendo una ciudad como capital, asegurarse de que las demás no lo sean
 		UPDATE Ciudad
 			SET CapitalRegion=0
 			FROM Ciudad C
@@ -14,3 +28,23 @@ AS BEGIN
 		
 	END
 END
+GO
+
+--version que deja la ultima capital de region ante ambiguedades
+ALTER TRIGGER tActualizarCapitalRegion
+ON Ciudad
+FOR INSERT, UPDATE
+AS BEGIN
+	-- Asegurarnos de que solo quede una ciudad como CapitalRegion = 1 por cada IdRegion
+	WITH UltimaCapital AS (SELECT Id, IdRegion
+							FROM inserted
+							WHERE CapitalRegion=1)
+	UPDATE C
+		SET C.CapitalRegion=CASE WHEN C.Id=U.Id THEN 1
+								ELSE 0
+							END
+		FROM Ciudad C
+		JOIN UltimaCapital U ON U.IdRegion = C.IdRegion
+
+END
+GO
